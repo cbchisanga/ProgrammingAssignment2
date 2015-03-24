e27b0d22885a957a01ef131ad8e31ace1b001fb7
cbchisanga <cbchisanga@users.noreply.github.com>
2015-03-24 09:05:53 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,7 @@
 ### Assignment: Caching the Inverse of a Matrix
-
 ##Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
-
 ###Write the following functions:
-
 ##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-
 ##################################################################
 makeCacheMatrix <- function(x = numeric()) {
   m <- NULL
