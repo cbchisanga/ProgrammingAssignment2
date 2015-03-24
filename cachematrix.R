64c13b90757e16b66c4a680676f2f7ec5d165af8
cbchisanga <cbchisanga@users.noreply.github.com>
2015-03-24 08:57:45 +0200

    Update and rename cachematrix0.R to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,36 @@
+### Assignment: Caching the Inverse of a Matrix
+
+##Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
+
+###Write the following functions:
+
+##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+##################################################################
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    x <- stats::rnorm(16)
+    dim(x) <- c(4,4)
+    x
+    solve(x)
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- meatrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+}
+#################
+
+### The following function calculates the matrix of the special "cacheSolve" created with the above function. However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+###########
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+##Computing the inverse of a square matrix can be done with the solve function in R. For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+
 cacheSolve <- function(x, ...) {
   m <- x$getmean()
   if(!is.null(m)) {
@@ -11,5 +44,5 @@ cacheSolve <- function(x, ...) {
 }
 #### Get the inverse matrix of matrix x:
 solve(x) %*% x
-
+################
 
